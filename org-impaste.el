13398005a3c4d6e0f04de9c3be6ed32ed252fb58
zombie110year <zombie110year@outlook.com>
2022-06-24 21:28:40 +0800

    feat: detect file type, change LICENSE to GPLv2
    use the magic database (which under GPLv2)

--- org-impaste.el
+++ org-impaste.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: extensions files multimedia
 ;; Homepage: https://github.com/zombie110year/org-impaste
-;; Package-Requires: ((emacs "25.3"))
+;; Package-Requires: ((emacs "25.3")) ;; todo use multi-thread, up to 26.1
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -18,6 +18,7 @@
 ;;  Paste image to org mode
 ;;
 ;;; Code:
+
 (require 'org)
 ;; Load from org-impaste DLL
 (declare-function org-impaste--download-external "org-impaste")
@@ -38,14 +39,29 @@
 
 (setq org-impaste-storage-dir (file-truename "~/org-life/static/images/"))
 
-
 (defun org-impaste-download (url referer)
   "Download images from internet, need input `URL'.
 store image files into `org-impaste-storage-dir'
 and request by `REFERER', if it's not `*'"
   (interactive "simage url: \nsreferer: ")
-  (message "org-impaste downloaded: %s"
-    (org-impaste--download-external url org-impaste-storage-dir referer)))
+  (let* ((buffer (current-buffer))
+         (line (line-beginning-position))
+         (col (- (point) line))
+         ;; todo async
+         (impath (org-impaste--download-external url org-impaste-storage-dir referer)))
+    (message "org-impaste downloaded: %s" impath)
+    (org-impaste--insert-link impath buffer line col)))
+
+(defun org-impaste--insert-link (filename buffer line col)
+  "Insert `FILENAME' as org link format at position `BUFFER'/`LINE':`COL'.
+Then return the original position."
+  (with-current-buffer buffer
+    (save-excursion
+      (goto-line line)
+      (forward-char col)
+      (insert (format "#+attr_org: :width %dpx\n" 600))
+      (insert (format "[[file:%s]]" filename)))))
+
 
 (provide 'org-impaste)
 ;;; org-impaste.el ends here
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ use curl::Curl;
 use emacs::{defun, IntoLisp};
 use sha2::{Digest, Sha256};
 use std::{io::Write, path::PathBuf};
-
 emacs::plugin_is_GPL_compatible!();
 
 #[emacs::module(name = "org-impaste")]
@@ -38,16 +37,17 @@ fn download_(url: String, store: PathBuf, referer: String) -> Result<String, Err
     };
     let im = curl.get(&url)?;
     let filename = hex_filename(&im);
-    // todo 如果不能直接从 url 中获取后缀，则使用文件头推测
-    let fileext = url.split('.').last().unwrap();
+    let mime = tree_magic_mini::from_u8(&im[..]);
+    let fileext = mime.split("/").last().unwrap();
     let impath = store.join(format!("{filename}.{fileext}"));
     let infopath = store.join(format!("{filename}.txt"));
     std::fs::write(&impath, im)?;
-    std::fs::OpenOptions::new()
+    let mut log = std::fs::OpenOptions::new()
         .create(true)
         .append(true)
-        .open(&infopath)?
-        .write(url.as_bytes())?;
+        .open(&infopath)?;
+    log.write(url.as_bytes())?;
+    log.write(b"\n")?;
     return Ok(impath.to_str().unwrap().to_string());
 }
 
@@ -69,9 +69,10 @@ pub(crate) fn hex_filename(content: &[u8]) -> String {
 #[test]
 fn test_download() {
     let x = download_(
-        "https://o.acgpic.net/img-original/img/2022/06/19/00/00/13/99142922_p0.jpg".to_string(),
+        "https://tse1-mm.cn.bing.net/th/id/OIP-C.iDdvnaGvywxqBKurXHDKcAHaFj?pid=ImgDet&rs=1"
+            .to_string(),
         PathBuf::new().join("debug"),
-        "https://pixivic.com/".to_string(),
+        "".to_string(),
     )
     .unwrap();
     println!("{x}");
