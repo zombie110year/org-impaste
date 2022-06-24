4a5cd3bab1914d98b2895ab2d8e164b68f6c3a08
zombie110year <zombie110year@outlook.com>
2022-06-24 19:06:11 +0800

    refact: extract download_ method to test

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub(crate) mod curl;
 
+use anyhow::Error;
 use curl::Curl;
 use emacs::{defun, IntoLisp};
 use sha2::{Digest, Sha256};
@@ -24,6 +25,11 @@ fn download(
     store: String,
     referer: String,
 ) -> emacs::Result<emacs::Value<'_>> {
+    let impath = download_(url, PathBuf::from(store), referer)?;
+    return impath.into_lisp(&env);
+}
+
+fn download_(url: String, store: PathBuf, referer: String) -> Result<String, Error> {
     let curl = Curl::new().check_installed()?.default_options();
     let curl = if !referer.is_empty() {
         curl.referer(&referer)
@@ -34,15 +40,15 @@ fn download(
     let filename = hex_filename(&im);
     // todo 如果不能直接从 url 中获取后缀，则使用文件头推测
     let fileext = url.split('.').last().unwrap();
-    let impath = PathBuf::from(&store).join(format!("{filename}.{fileext}"));
-    let infopath = PathBuf::from(&store).join(format!("{filename}.txt"));
+    let impath = store.join(format!("{filename}.{fileext}"));
+    let infopath = store.join(format!("{filename}.txt"));
     std::fs::write(&impath, im)?;
     std::fs::OpenOptions::new()
         .create(true)
         .append(true)
         .open(&infopath)?
         .write(url.as_bytes())?;
-    return impath.to_str().into_lisp(&env);
+    return Ok(impath.to_str().unwrap().to_string());
 }
 
 /// paste image from clipboard
@@ -58,4 +64,15 @@ pub(crate) fn hex_filename(content: &[u8]) -> String {
     let hash = hasher.finalize();
     let hexname = format!("{:x}", hash);
     return hexname;
+}
+
+#[test]
+fn test_download() {
+    let x = download_(
+        "https://o.acgpic.net/img-original/img/2022/06/19/00/00/13/99142922_p0.jpg".to_string(),
+        PathBuf::new().join("debug"),
+        "https://pixivic.com/".to_string(),
+    )
+    .unwrap();
+    println!("{x}");
 }
