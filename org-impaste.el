4cb419f0014bc7e8e2cfd50a0b4dbcc7908a931a
zombie110year <zombie110year@outlook.com>
2022-06-28 14:30:43 +0800

    feat: org-impaste-download command in emacs

--- org-impaste.el
+++ org-impaste.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: extensions files multimedia
 ;; Homepage: https://github.com/zombie110year/org-impaste
-;; Package-Requires: ((emacs "25.3")) ;; todo use multi-thread, up to 26.1
+;; Package-Requires: ((emacs "26.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -37,31 +37,19 @@
   "The directory to store all the image files."
   :type 'string)
 
-(setq org-impaste-storage-dir (file-truename "~/org-life/static/images/"))
+(setq org-impaste-storage-dir (file-truename "./debug/"))
 
 (defun org-impaste-download (url referer)
   "Download images from internet, need input `URL'.
 store image files into `org-impaste-storage-dir'
 and request by `REFERER', if it's not `*'"
   (interactive "simage url: \nsreferer: ")
-  (let* ((buffer (current-buffer))
-         (line (line-beginning-position))
-         (col (- (point) line))
-         ;; todo async
-         (impath (org-impaste--download-external url org-impaste-storage-dir referer)))
-    (message "org-impaste downloaded: %s" impath)
-    (org-impaste--insert-link impath buffer line col)))
-
-(defun org-impaste--insert-link (filename buffer line col)
-  "Insert `FILENAME' as org link format at position `BUFFER'/`LINE':`COL'.
-Then return the original position."
-  (with-current-buffer buffer
-    (save-excursion
-      (goto-line line)
-      (forward-char col)
-      (insert (format "#+attr_org: :width %dpx\n" 600))
-      (insert (format "[[file:%s]]" filename)))))
-
+  ;; todo get returned impath and insert format link into buffer
+  (make-thread
+   (lambda ()
+     (message "org-impaste downloaded: %s"
+              (org-impaste--download-external url org-impaste-storage-dir referer))))
+  (message "org-impaste downloading: %s" url))
 
 (provide 'org-impaste)
 ;;; org-impaste.el ends here
